a78891d3b9040f17cc4b0ad8905d60388c0c7ce3
NiceHairs <david-schwab@outlook.de>
2023-03-04 17:28:50 +0100

    Improvement File Dialog

--- ImageRater/MainWindow.xaml.cs
+++ ImageRater/MainWindow.xaml.cs
@@ -1,9 +1,8 @@
 ﻿namespace ImageRater;
 
-using System.IO;
+using ImageRater.ViewModels;
 using System.Windows;
 using System.Windows.Forms;
-using ImageRater.ViewModels;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
@@ -14,16 +13,30 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         using (var dialog = new FolderBrowserDialog())
-
-            while (!Directory.Exists(dialog.SelectedPath))
+        {
+            dialog.Description = "Please select the folder that contains your input images.";
+            dialog.UseDescriptionForTitle = true;
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-                {
-                    this.ImageRater = new(dialog.SelectedPath);
-                    this.DataContext = this.ImageRater;
-                }
+                this.ImageRater = new(dialog.SelectedPath);
+                this.DataContext = this.ImageRater;
+                this.ContentRendered += MainWindow_ContentRendered;
             }
+            else
+            {
+                this.Close();
+            }
+        }
 
         InitializeComponent();
+    }
+
+    // This event handler is called after the MainWindow's content has been rendered. 
+    // It sets the Topmost property of the MainWindow to true and then false, 
+    // so that the window is brought to the front and stays on top after everything is rendered.
+    private void MainWindow_ContentRendered(object? sender, System.EventArgs e)
+    {
+        this.Topmost = true;
+        this.Topmost = false;
     }
 }
